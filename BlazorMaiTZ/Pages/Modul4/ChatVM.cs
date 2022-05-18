365e45e36b8c8ed3174e85026a18917df46b7d6e
Hannes Preishuber <hannesp@ppedv.de>
2022-05-18 12:08:52 +0200

    Chat Viewmodel mit Event Notification und Liste

---
+++ BlazorMaiTZ/Pages/Modul4/ChatVM.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorMaiTZ.Pages.Modul4
+{
+    public class ChatVM
+    {
+        public Action OnAdd;
+        public List<NachrichtModel> NachrichtenListe { get; set; }=
+            new List<NachrichtModel>();
+        public void Add(string msg)
+        {
+            NachrichtenListe.Add(new NachrichtModel() { Text = msg });
+            OnAdd?.Invoke();    
+        }
+    }
+}
