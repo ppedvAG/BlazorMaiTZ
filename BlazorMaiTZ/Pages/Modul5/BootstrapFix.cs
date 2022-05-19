f9df9dcfda02d3f24cdf43ec8b6bf8269cc46f9a
Hannes Preishuber <hannesp@ppedv.de>
2022-05-19 11:31:45 +0200

    Boostrap CSS Fix invalid vs is-invalid

---
+++ BlazorMaiTZ/Pages/Modul5/BootstrapFix.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorMaiTZ.Pages.Modul5
+{
+    public class BootstrapFix : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext,
+            in FieldIdentifier fieldIdentifier)
+        {
+            var isinvalid=editContext.GetValidationMessages(fieldIdentifier )
+                .Any();
+            return isinvalid ? "is-invalid" : "is-valid";
+        }
+    }
+}
