43f661a55bf0e2f49f4040433762648bd236eb4b
Hannes Preishuber <hannesp@ppedv.de>
2022-05-19 11:21:03 +0200

    Eingabeformular mit Validierung

---
+++ BlazorMaiTZ/Pages/Modul5/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMaiTZ.Pages.Modul5
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="bitte ID")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "bitte Name")]
+        [StringLength(10,ErrorMessage ="max 10")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "bitte Größe")]
+        [Range(140,199,ErrorMessage ="keine Achterbahn")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "bitte Gebdat")]
+        public DateTime GebDat { get; set; }
+    }
+}
