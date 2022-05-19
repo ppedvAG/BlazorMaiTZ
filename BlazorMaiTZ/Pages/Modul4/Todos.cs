557fcaf33858c1dedce953e4c426fc84356d3369
Hannes Preishuber <hannesp@ppedv.de>
2022-05-19 09:06:16 +0200

    REST Model HttpClient

---
+++ BlazorMaiTZ/Pages/Modul4/Todos.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorMaiTZ.Pages.Modul4
+{
+    public class Todos
+    {
+
+
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
