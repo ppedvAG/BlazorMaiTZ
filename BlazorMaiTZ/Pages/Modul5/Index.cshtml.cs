8b0d89d909af4d25add8b1029e58f62f297f365b
Hannes Preishuber <hannesp@ppedv.de>
2022-05-19 10:22:28 +0200

    HTML INput Web Regeln

---
+++ BlazorMaiTZ/Pages/Modul5/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorMaiTZ.Pages.Modul5
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
