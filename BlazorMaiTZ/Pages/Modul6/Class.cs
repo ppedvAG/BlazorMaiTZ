80e6c1a50f56cfe94c9cd7e58066b47329e5e4ea
Hannes Preishuber <hannesp@ppedv.de>
2022-05-20 08:40:22 +0200

    Bindable Component mit Bug

---
+++ BlazorMaiTZ/Pages/Modul6/Class.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorMaiTZ.Pages.Modul6
+{
+    public class Class
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+    }
+}
